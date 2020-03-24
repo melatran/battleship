01cfb99a6d626e445879c21e696e2fd4d3795e16
Melanie Tran <melaniexx02@gmail.com>
2020-03-23 20:30:12 -0600

    Created a test for the ship's attributes: name and length

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,6 +9,10 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
-  # def test_it_has_attributes
-  #   cruiser = Ship.new("Cruiser", 3)
+  def test_it_has_attributes
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+  end
 end
