05fc651e392cef39146dd0ae78c7863f79fd07d9
Melanie Tran <melaniexx02@gmail.com>
2020-03-24 08:46:39 -0600

    Created hit and sunk? method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,13 +1,27 @@
 class Ship
   attr_reader :name,
-              :length
+              :length,
+              :health
 
   def initialize(name, length)
     @name = name
     @length = length
+    @health = health
   end
 
   def health
     health = @length
+  end
+
+  def hit
+    @length -= 1
+  end
+
+  def sunk?
+    if health == 0
+      true
+    else
+      false
+    end
   end
 end
