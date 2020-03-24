62d30baa93566dbd25f15addf32e61cc496708d0
Melanie Tran <melaniexx02@gmail.com>
2020-03-24 17:49:08 -0600

    Created board class and board test

---
+++ lib/board.rb
@@ -0,0 +1,12 @@
+class Board
+  # attr_reader :cell
+  #
+  # def initialize
+  #   @cell = cell
+  # end
+  #
+  # def put_cells_on_board
+  #   coordinates = {A1 :cell1, A2 :cell2, A3 :cell3, A4 :cell4, B1 :cell5, B2 :cell6, B3 :cell7, B4 :cell8, C1 :cell9, C2 :cell10, C3 :cell11, C4 :cell12, D1 :cell13, D2 :cell14, D3 :cell15, D4 :cell16}
+  #
+  # end
+end
---
+++ test/board_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship.rb'
+require './lib/cell.rb'
+require './lib/board.rb'
+
+class BoardTest < Minitest::Test
+  # def test_board_exists
+  #   board = Board.new
+  #   assert_equal Board, board
+  # end
+end
