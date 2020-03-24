0aa1f9f308aa9583ddeb06e97220b55f75577738
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-24 12:52:15 -0600

    Create .place_ship method and pass test_can_place_ship

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,6 +6,7 @@ require './lib/cell.rb'
 class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
@@ -19,5 +20,11 @@ class CellTest < Minitest::Test
 
   def test_cell_starts_empty
     assert_equal true, @cell.empty?
+  end
+
+  def test_can_place_ship
+    @cell.place_ship(@cruiser)
+    assert_equal @cruiser, @cell.ship
+    assert_equal false, @cell.empty?
   end
 end
