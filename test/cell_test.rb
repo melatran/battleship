1db1fcb772df6e60821246ca89696a90a46b420d
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-24 13:08:43 -0600

    Create test_it_can_render

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,6 +6,8 @@ require './lib/cell.rb'
 class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("B4")
+    @cell2 = Cell.new("C3")
+
     @cruiser = Ship.new("Cruiser", 3)
   end
 
@@ -22,17 +24,35 @@ class CellTest < Minitest::Test
     assert_equal true, @cell.empty?
   end
 
-  def test_can_place_ship
+  def test_it_can_place_ship
     @cell.place_ship(@cruiser)
     assert_equal @cruiser, @cell.ship
     assert_equal false, @cell.empty?
   end
 
-  def test_can_be_fired_upon
+  def test_it_can_be_fired_upon
     @cell.place_ship(@cruiser)
     assert_equal false, @cell.fired_upon?
 
     @cell.fire_upon
     assert_equal true, @cell.fired_upon?
+  end
+
+  def test_it_can_render
+    assert_equal ".", @cell.render
+
+    @cell.fire_upon
+    assert_equal "M", @cell.render
+
+    @cell2.place_ship(@cruiser)
+    assert_equal "S", @cell2.render(true)
+
+    @cell2.fire_upon
+    assert_equal "H", @cell2.render
+
+    @cruiser.hit
+    @cruiser.hit
+
+    assert_equal "X", @cell2.render
   end
 end
