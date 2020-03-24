d96e196eaf08ea3efcc6465b1d36f56d1de4b239
Melanie Tran <melaniexx02@gmail.com>
2020-03-23 20:25:32 -0600

    Initialized attributes of the ship class

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,4 +3,12 @@ require 'minitest/pride'
 require './lib/ship.rb'
 
 class ShipTest < Minitest::Test
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+  end
+
+  # def test_it_has_attributes
+  #   cruiser = Ship.new("Cruiser", 3)
 end
