e4c9a2d26e599d0bd728848882807178afdf5140
Melanie Tran <melaniexx02@gmail.com>
2020-03-24 20:13:42 -0600

    Created cells in a hash

--- lib/board.rb
+++ lib/board.rb
@@ -18,5 +18,23 @@ class Board
       cell14 = Cell.new("D2"),
       cell15 = Cell.new("D3"),
       cell16 = Cell.new("D4")
+
+      @cells = {"A1" => cell1,
+              "A2" => cell2,
+              "A3" => cell3,
+              "A4" => cell4,
+              "B1" => cell5,
+              "B2" => cell6,
+              "B3" => cell7,
+              "B4" => cell8,
+              "C1" => cell9,
+              "C2" => cell10,
+              "C3" => cell11,
+              "C4" => cell12,
+              "D1" => cell13,
+              "D2" => cell14,
+              "D3" => cell15,
+              "D4" => cell16
+            }
   end
 end
