a408758e6eaa62a252a1686775d40c8da3d772a4
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-25 16:19:21 -0600

    Separate tests out for readability, pass test_can_place_horizontal and test_can_place_vertical

--- test/board_test.rb
+++ test/board_test.rb
@@ -37,6 +37,14 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
     assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
     assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+  end
+
+  def test_can_place_horizontal
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+  end
+
+  def test_can_place_vertical
+    assert_equal true, @board.valid_placement?(@submarine, ["B1", "C1"])
   end
 end
