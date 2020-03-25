c308ef14c8dc926392569398b7b9f5d8f3f77ce0
Melanie Tran <melaniexx02@gmail.com>
2020-03-25 07:51:31 -0600

    Refactor initialize for class board

--- lib/board.rb
+++ lib/board.rb
@@ -1,40 +1,31 @@
 class Board
-  attr_reader :cells,
+  attr_reader :cells, :coordinates
 
   def initialize
-      cell1 = Cell.new("A1"),
-      cell2 = Cell.new("A2"),
-      cell3 = Cell.new("A3"),
-      cell4 = Cell.new("A4"),
-      cell5 = Cell.new("B1"),
-      cell6 = Cell.new("B2"),
-      cell7 = Cell.new("B3"),
-      cell8 = Cell.new("B4"),
-      cell9 = Cell.new("C1"),
-      cell10 = Cell,new("C2"),
-      cell11 = Cell.new("C3"),
-      cell12 = Cell.new("C4"),
-      cell13 = Cell.new("D1"),
-      cell14 = Cell.new("D2"),
-      cell15 = Cell.new("D3"),
-      cell16 = Cell.new("D4")
+      @cells = {}
+      @cells = Hash.new
+      @coordinates =["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4",
+            "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+  end
 
-      @cells = {"A1" => cell1,
-              "A2" => cell2,
-              "A3" => cell3,
-              "A4" => cell4,
-              "B1" => cell5,
-              "B2" => cell6,
-              "B3" => cell7,
-              "B4" => cell8,
-              "C1" => cell9,
-              "C2" => cell10,
-              "C3" => cell11,
-              "C4" => cell12,
-              "D1" => cell13,
-              "D2" => cell14,
-              "D3" => cell15,
-              "D4" => cell16
-            }
+  def cells
+    @cells = @coordinates.map do |coordinate|
+      
   end
 end
+# cell1 = Cell.new("A1"),
+# cell2 = Cell.new("A2"),
+# cell3 = Cell.new("A3"),
+# cell4 = Cell.new("A4"),
+# cell5 = Cell.new("B1"),
+# cell6 = Cell.new("B2"),
+# cell7 = Cell.new("B3"),
+# cell8 = Cell.new("B4"),
+# cell9 = Cell.new("C1"),
+# cell10 = Cell,new("C2"),
+# cell11 = Cell.new("C3"),
+# cell12 = Cell.new("C4"),
+# cell13 = Cell.new("D1"),
+# cell14 = Cell.new("D2"),
+# cell15 = Cell.new("D3"),
+# cell16 = Cell.new("D4")
