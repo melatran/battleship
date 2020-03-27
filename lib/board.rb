3b722ad806ac1040e1558937580ffe73730de2a6
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-27 15:59:23 -0600

    Create render method and pass test_it_can_render

--- lib/board.rb
+++ lib/board.rb
@@ -75,4 +75,13 @@ class Board
         @cells[coordinate].place_ship(ship)
     end
   end
+
+  def render(show_ship = false)
+    board_render = "  1 2 3 4 \n" +
+    "A #{cells["A1"].render(show_ship)} #{cells["A2"].render(show_ship)} #{cells["A3"].render(show_ship)} #{cells["A4"].render(show_ship)} \n" +
+    "B #{cells["B1"].render(show_ship)} #{cells["B2"].render(show_ship)} #{cells["B3"].render(show_ship)} #{cells["B4"].render(show_ship)} \n" +
+    "C #{cells["C1"].render(show_ship)} #{cells["C2"].render(show_ship)} #{cells["C3"].render(show_ship)} #{cells["C4"].render(show_ship)} \n" +
+    "D #{cells["D1"].render(show_ship)} #{cells["D2"].render(show_ship)} #{cells["D3"].render(show_ship)} #{cells["D4"].render(show_ship)} \n"
+    board_render
+  end
 end
