dd1c661d159246104dd6b4c1913d004fc67bbade
Melanie Tran <melaniexx02@gmail.com>
2020-03-29 17:02:43 -0600

    Removed minitest from runner file

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,10 +1,7 @@
-require 'minitest/autorun'
-require 'minitest/pride'
 require './lib/ship.rb'
 require './lib/cell.rb'
 require './lib/board.rb'
 require './lib/game.rb'
-require "pry"
 
 game = Game.new
-game.main_menu
+game.start
