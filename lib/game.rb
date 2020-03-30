e2e189358782792666b743a2d9f719033348a5b1
Melanie Tran <melaniexx02@gmail.com>
2020-03-29 18:14:48 -0600

    Add Computer and Human instance variables

--- lib/game.rb
+++ lib/game.rb
@@ -1,26 +1,27 @@
 class Game
 
   def initialize
-    # @computer_data = computer_data
-    # @user_data = user_data
+    @computer_board = Board.new
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
+    @human_board = Board.new
+    @human_cruiser = Ship.new("Cruiser", 3)
+    @human_submarine = Ship.new("Submarine", 2)
   end
 
   def main_menu
-    require "pry";binding.pry
     p "Welcome to BATTLESHIP!"
     p "Enter p to play. Enter q to quit."
     user_input = gets.chomp.downcase
     if user_input == 'p'
       setup
     else
+      p "Invalid Input"
       exit
     end
   end
 
-  def setup
-    p "Setup test"
-  end
-
-  def exit
+  def set_the_game
+    p "test"
   end
 end
