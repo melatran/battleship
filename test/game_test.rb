6c37da252eed2f1ed842592d3a03381428c617c5
Melanie Tran <melaniexx02@gmail.com>
2020-03-29 18:15:16 -0600

    Edit setup of test to include computer and human

--- test/game_test.rb
+++ test/game_test.rb
@@ -6,8 +6,17 @@ require './lib/board.rb'
 require './lib/game.rb'
 
 class GameTest < Minitest::Test
+  def setup
+    @computer_board = Board.new
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
+    @human_board = Board.new
+    @human_cruiser = Ship.new("Cruiser", 3)
+    @human_submarine = Ship.new("Submarine", 2)
+    @game = Game.new
+  end
+
   def test_game_exits
-    game = Game.new
-    assert_instance_of Game, game
+    assert_instance_of Game, @game
   end
 end
